e97ef155291d51867f3db2f30157cb7f197d22dd
Diego Andrés Alvarez Marín <diegotorquemada@gmail.com>
2026-09-10 07:58:15 -0500

    Better names for some variables

--- codigo/01_approximation_and_round_off_errors/01_error_iterative.py
+++ codigo/01_approximation_and_round_off_errors/01_error_iterative.py
@@ -1,15 +1,15 @@
 from math import exp, factorial
 
 def calculate_taylor_series_exp(n, x):
-    approx = 0
+    s = 0
     for i in range(n):
-        approx += (x**i)/factorial(i)
-    return approx
+        s += (x**i)/factorial(i)
+    return s
 
 x = 0.5
 true_value = exp(x)
 max_terms = 10
-approx = float('nan')
+current_approx = math.nan
 e_tol  = 0.5e-5  # n=5 digits
 
 print(f"Actual value of exp({x}) = {true_value}")
@@ -17,14 +17,14 @@ print("|  n |  Approximation | Absolute Error | Relative Err 1 | Relative Err 2 
 print("|----|----------------|----------------|----------------|----------------|")
 
 for n in range(1, max_terms + 1):
-    previous_approx = approx
+    previous = current_approx
 
-    approx   = calculate_taylor_series_exp(n, x)
-    abs_err  = abs(true_value - approx)
-    rel_err1 = abs((true_value - approx)/true_value)
-    rel_err2 = (approx - previous_approx)/approx
+    current_approx   = calculate_taylor_series_exp(n, x)
+    abs_err  = abs(true_value - current_approx)
+    rel_err1 = abs((true_value - current_approx)/true_value)
+    rel_err2 = (current_approx - previous)/current_approx
 
-    print(f"| {n:2d} | {approx:.12f} | {abs_err:.12f} | {rel_err1:.12f} | {rel_err2:.12f} |")
+    print(f"| {n:2d} | {current_approx:.12f} | {abs_err:.12f} | {rel_err1:.12f} | {rel_err2:2.12e} |")
 
     if abs(rel_err2) < e_tol:
         break
